b48a849f6f5bcf65a3f561db82c1a0d8e2e94f51
Jake Alvarez <yakisoba@Jakes-iMac.local>
2022-01-25 17:42:51 -0600

    changed name of file. Added message to answer for real.

--- assessment 3/runnerup_score.py
+++ assessment 3/runnerup_score.py
@@ -6,4 +6,6 @@ n = int(input())
 print("Please enter individual scores: ")
 arr = list(map(int, input().split()))
 arr.sort()
-print("The runner up score is: " + arr[(arr.index(max(arr)))-1])
+
+print("The runner up score is: ")
+print(arr[(arr.index(max(arr)))-1])
